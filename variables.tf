f300efe34277f6e5d6cccd92c0ff2c8101bba75f
Mbeh2023 <mbehs@yahoo.com>
2023-11-11 09:17:44 -0500

    terraform

---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "ami" {
+    description = "amazon machine image"
+    type = string
+  default = "ami-0e8a34246278c21e4"
+}
+variable "instance_type" {
+  default = "t2.small"
+}
+variable "region_name" {
+    #default = "us-east-1"
+  
+}
+variable "profile_name" {
+    default = "default"
+  
+}
